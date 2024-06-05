11d826ea3b546d912478520512d53a82c4a3c6d5
Abhay Gupta <abhay19021@gmail.com>
2024-06-05 10:16:18 +0530

    modified something

--- controllers/database.mjs
+++ controllers/database.mjs
@@ -1,6 +1,7 @@
-import { MongoClient } from "mongodb";
 import dotenv from "dotenv"
 const result = dotenv.config()
+
+import { MongoClient } from "mongodb";
 const uri = `mongodb+srv://${process.env.SA_USERNAME}:${process.env.SA_PASS}@pustak-main.h6gghsg.mongodb.net/?retryWrites=true&w=majority&appName=pustak-main
 `
 const client = new MongoClient(uri);
